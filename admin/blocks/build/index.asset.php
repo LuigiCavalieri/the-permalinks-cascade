ac7ae4fc3ed26c94bd2c163905dd75a8b2b072b0
Luigi Cavalieri <58509573+LuigiCavalieri@users.noreply.github.com>
2022-01-08 17:38:37 +0100

    Converted px to em in CustomPostEdit.css

--- admin/blocks/build/index.asset.php
+++ admin/blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '0e5adf2229e0616b3684cee1017d9baf');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '6720b6366c4ec3e57a8c875344796446');
